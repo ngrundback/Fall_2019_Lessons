bf4e9cc29cdf1be7615d934ab93b5b70aea1f34b
Kyle <kyle1james@gmail.com>
2019-05-20 13:03:37 -0400

    meow

--- heuristic/dijkstra.py
+++ heuristic/dijkstra.py
@@ -24,7 +24,7 @@ def path_finder(matrix):
         px, py = x,y
 
         if (x,y) == goal:
-            return (visited[goal] + matrix[x][y])
+            return (visited[goal])
 
 
         neighbors = ((x-1,y),(x,y-1),(x+1,y),(x,y+1))
@@ -41,7 +41,16 @@ def path_finder(matrix):
 
 
 import random, pprint
-matrix = [[x + 1 for x in range(10)] for y in range(10)]
+matrix = [[0, 6, 6, 2, 8, 9, 9, 3, 8, 3],
+        [6, 5, 2, 9, 5, 7, 9, 1, 7, 7],
+        [3, 9, 5, 1, 4, 3, 0, 1, 8, 3],
+        [4, 7, 2, 6, 4, 8, 0, 2, 9, 2],
+        [3, 5, 0, 6, 0, 1, 1, 2, 6, 9],
+        [9, 8, 5, 8, 3, 2, 4, 9, 9, 0],
+        [9, 7, 7, 9, 1, 1, 5, 0, 2, 5],
+        [1, 4, 4, 3, 3, 4, 1, 9, 4, 4],
+        [5, 5, 2, 0, 9, 6, 2, 7, 3, 3],
+        [8, 2, 4, 8, 2, 2, 2, 3, 1, 6]]
 # with open('matrix.txt', 'w') as f:
 #     for item in matrix:
 #         f.write("%s\n," % item)
