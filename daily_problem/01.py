f6c1b6af015e993b4c00b694c0271b770d3988dd
kyle <kyle1james@gmail.com>
2019-05-28 18:05:20 -0400

    meow

--- daily_problem/01.py
+++ daily_problem/01.py
@@ -1,6 +1,7 @@
-# given an array, return true if any two numbers add up to a give number
+# given an array arr and num k, return true if any two numbers in arr add up to a given number k.
+
 # O(n2)
-def any_two(arr, num):
+def nested(arr, num):
     ans = []
     length = len(arr)
     for x in range(length-1):
@@ -8,18 +9,15 @@ def any_two(arr, num):
             if arr[x]+arr[y] == num:
                 ans.append((arr[x],arr[y]))
     return ans
-my_nums = [10,15,3,7]
-print(any_two(my_nums, 17))
 
 # O(n)
-def same_num(arr,num):
+def count_down(arr,num):
     for x in range(num):
         y = num - x
         if x in arr and y in arr:
             return True
     return False
 
-print(same_num(my_nums, 17))
 
 def linear_time(arr,num):
     for x in arr:
@@ -28,14 +26,19 @@ def linear_time(arr,num):
             return True
     return False
 
-print(linear_time(my_nums, 17))
 
 #O(n/2)
 def half_time(arr, num):
-    for x in range(len(arr)//2+1):
+    new_list = range(len(arr)//2+1)
+    for x in new_list:
         num2 = num - x
         if num2 in arr:
             return True
     else:
         return False
+
+my_nums = [10,15,3,7]
+print(nested(my_nums, 17))
+print(count_down(my_nums, 17))
+print(linear_time(my_nums, 17))
 print(half_time(my_nums, 17))
