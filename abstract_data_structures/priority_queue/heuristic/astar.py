14f428a76d51ae691572d86a5e70ab772f182298
Kyle <kyle1james@gmail.com>
2019-05-29 08:45:46 -0400

    meow

--- abstract_data_structures/priority_queue/heuristic/astar.py
+++ abstract_data_structures/priority_queue/heuristic/astar.py
@@ -18,10 +18,10 @@ def path_finder(matrix):
         #unseenNodes = sorted(unseenNodes, key = lambda x: x[3])
         #minNode = unseenNodes.pop(0)
 
-        x,y,current_weight,_ = minNode
+        current_weight, distance, x, y = minNode
         px, py = x,y
 
-        neighbors = ((x-1,y),(x,y-1),(x+1,y),(x,y+1), (x-1, y-1), (x+1, y+1), (x-1,y+1), (x+1, y-1))
+        neighbors = ((x-1,y),(x,y-1),(x+1,y),(x,y+1))
         real_neighbors = ((x,y) for (x,y) in neighbors if 0 <= x < length and 0<= y < len(matrix[0]))
 
         for cx,cy in real_neighbors:
@@ -29,15 +29,17 @@ def path_finder(matrix):
             dist = abs(goal[0] - cx) + abs(goal[1]-cy)
             if (cx,cy) not in parent or cost < visited[(cx,cy)]:
                 visited[(cx,cy)] = cost
-                unseenNodes.append((cx,cy,cost,dist))
+                unseenNodes.append((cost,dist,cx,cy))
                 parent[(cx,cy)] = (px,py)
 
-                if (x,y) == goal:
-                    return (visited[goal] + matrix[x][y])
+                if (cx,cy) == goal:
+                    print('hey')
+                    return (visited[goal])
 
 
 import random, pprint
-matrix = [[x + 1 for x in range(100000)] for y in range(100000)]
+matrix = [[x + 1 for x in range(5)] for y in range(5)]
+pprint.pprint(matrix)
 start_time = time.time()
 print(path_finder(matrix))
 print("--- %s seconds ---" % (time.time() - start_time))
