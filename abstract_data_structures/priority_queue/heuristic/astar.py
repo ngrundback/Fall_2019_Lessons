973d47c4dcbfbd29e3a19d17764f8be29b9c0fba
Kyle <kyle1james@gmail.com>
2019-05-29 13:11:30 -0400

    meow

--- abstract_data_structures/priority_queue/heuristic/astar.py
+++ abstract_data_structures/priority_queue/heuristic/astar.py
@@ -18,36 +18,35 @@ def path_finder(matrix):
         #unseenNodes = sorted(unseenNodes, key = lambda x: x[3])
         #minNode = unseenNodes.pop(0)
 
-        current_weight, distance, x, y = minNode
+        distance, current_weight, x, y = minNode
         px, py = x,y
 
-        neighbors = ((x-1,y),(x,y-1),(x+1,y),(x,y+1), (x-1,y-1), (x+1,y+1))
-        real_neighbors = ((x,y) for (x,y) in neighbors if 0 <= x < length and 0<= y < len(matrix[0]))
+        neighbors = ((x-1,y),(x,y-1),(x+1,y),(x,y+1), (x-1,y-1), (x+1,y+1), (x-1,y+1), (x+1,y-1))
+        real_neighbors = ((x,y) for (x,y) in neighbors if 0 <= x < length and 0<= y < length)
 
         for cx,cy in real_neighbors:
             cost = current_weight + matrix[cx][cy]
             dist = abs(goal[0] - cx) + abs(goal[1]-cy)
             if (cx,cy) not in parent or cost < visited[(cx,cy)]:
                 visited[(cx,cy)] = cost
-                unseenNodes.append((cost,dist,cx,cy))
+                heapq.heappush(unseenNodes,(dist,cost,cx,cy))
+                #unseenNodes.append((ccost,cost,cx,cy))
                 parent[(cx,cy)] = (px,py)
 
                 if (cx,cy) == goal:
                     print('hey, hey, hey')
 
-                    currentNode = (length-1,length-1)
-                    while currentNode != (0,0):
-                        x,y = currentNode
-                        matrix[x][y] = "X"
-                        currentNode = parent[(x,y)]
-                    matrix[0][0] = 'X'
-                    pprint.pprint(matrix)
+                    # currentNode = (length-1,length-1)
+                    # while currentNode != (0,0):
+                    #     x,y = currentNode
+                    #     matrix[x][y] = "X"
+                    #     currentNode = parent[(x,y)]
+                    # matrix[0][0] = 'X'
                     return (visited[goal])
 
 
 import random, pprint
-matrix = [[x + 1 for x in range(5)] for y in range(5)]
-pprint.pprint(matrix)
+matrix = [[x + 1 for x in range(10000)] for y in range(10000)]
 start_time = time.time()
 print(path_finder(matrix))
 print("--- %s seconds ---" % (time.time() - start_time))
