189b7bbbc80bb945f33c8e0dbfe7facae35d912a
Kyle <kyle1james@gmail.com>
2019-06-06 08:16:18 -0400

    meow

--- daily_problem/06_highest_nonadjacent_nums.py
+++ daily_problem/06_highest_nonadjacent_nums.py
@@ -1,28 +1,16 @@
 # given a list of intergers return the highest
 # nonadjacent sum
 import pprint
-nums = [2,4,6,5,10,3]
-# def is_safe(matrix, value1, value2):
-#     if matrix[value1][value2] == 1 and matrix[value2][value1] == 1:
-#         return False
-#     return True
-#
-# def find_sum(nums):
-#     # make adjaceny matrix
-#     hi = max(nums)+1
-#     matrix = [[0 for x in range(hi)]for y in range(hi)]
-#     for x in range(len(nums)-1):
-#         matrix[nums[x]][nums[x+1]] = 1
-#         matrix[nums[x+1]][nums[x]] = 1
-#     pprint.pprint(matrix)
-#     t = is_safe(matrix,2,5)
-#     print(t)
-#
-#
-# print(find_sum(nums))
+nums = [2,4,6,5]
 
+def find_sum(arr):
+    incl = 0
+    excl = 0
 
+    for x in arr:
+        new_excl = max(incl,excl)
+        incl = excl + x
+        excl = new_excl
+    return max(incl,excl)
 
-def sums(nums):
-
-print(sums(nums))
+print(find_sum(nums))
