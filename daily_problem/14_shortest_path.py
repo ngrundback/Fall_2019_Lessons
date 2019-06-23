dd7795fdf39d73e7396369f040a6ba2d58767327
kyle <kyle1james@gmail.com>
2019-06-23 10:21:38 -0400

    meow

--- daily_problem/14_shortest_path.py
+++ daily_problem/14_shortest_path.py
@@ -1,19 +1,21 @@
 import random
 import pprint
 
-matrix = [[random.randint(0,9) for x in range(5)] for y in range(5)]
+def make_matrix(size, lvl):
+    matrix = [[random.randint(0,lvl) for x in range(size)] for y in range(size)]
+    return matrix
 
-def is_safe(visited, length, cx, cy):
-    if (cx,cy) not in visited and 0<= cx < length and 0<= cy <length:
+
+def is_safe(visited, length, rlength, cx, cy):
+    if (cx,cy) not in visited and 0<= cx < length and 0<= cy < rlength:
         return True
 
-
-def shortest_path(matrix, start):
+def shortest_path(matrix, start, goal):
     length = len(matrix)
+    rlength = len(matrix[0])
     queue = [start]
     cache = {start:0}
     visited = []
-    goal = (4,4)
 
     while queue:
         x,y = queue.pop(0)
@@ -21,7 +23,7 @@ def shortest_path(matrix, start):
         px,py = x,y
 
         for cx,cy in (x+1, y), (x-1, y), (x, y+1), (x, y-1):
-            if is_safe(visited, length, cx, cy):
+            if is_safe(visited, length, rlength, cx, cy):
                 if matrix[cx][cy] > 2:
                     queue.append((cx,cy))
                     cache[(cx,cy)] = (px,py)
@@ -43,8 +45,19 @@ def steps_count(cache, matrix,goal,start):
     return steps
 
 if __name__  == '__main__':
-    cache = shortest_path(matrix,(0,0))
+    size = int(input('what size maze'))
+    lvl = int(input('what lvl maze'))
+    startx = int(input('what is start x'))
+    starty = int(input('what is start y'))
+    start = (startx,starty)
+    goalx = int(input('what is goal x'))
+    goaly = int(input('what is goal y'))
+    goal = (goalx,goaly)
+
+
+    matrix = make_matrix(size,lvl)
+    cache = shortest_path(matrix,start, goal)
     if cache != False:
-        print(steps_count(cache, matrix, (4,4), (0,0)))
+        print(steps_count(cache, matrix, goal, start))
     else:
         print(False)
